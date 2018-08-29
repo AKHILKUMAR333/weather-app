298f755337bafc76983cc56b1562067506a4d4e3
akhil <golukumar2027@gmail.com>
2018-08-29 13:40:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'b05d77fdc88cbbd01d3509c184fea136';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
+
